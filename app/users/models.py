05f6e3469b82dfc59bbaea5d388404187f3eb356
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2023-07-07 01:27:47 -0500

    update docker fiels to update files in container

--- app/users/models.py
+++ app/users/models.py
@@ -1,6 +1,6 @@
 from database import db
-from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import TIMESTAMP
+# from sqlalchemy.sql.expression import text
+# from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,8 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String)
-    created_at = db.Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
+    # created_at = db.Column(TIMESTAMP(timezone=True),
+    #                     nullable=False, server_default=text('now()'))
 
     def __repr__(self):
        return f"<User(name='{self.name}', email='{self.email}')>"
